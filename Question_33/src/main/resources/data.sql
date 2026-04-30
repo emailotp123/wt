e90ccec03eeb416a6717eb6522ebc81724d7e90e
Vedant Mahalle <vedantmahalle39@gmail.com>
2026-04-30 19:30:24 +0530

    Solve Question 33: Login attempt tracking with Spring Boot

---
+++ Question_33/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (username, password, account_non_locked, failed_attempt, lock_time) 
+VALUES ('admin', '$2a$10$P2N58zT00.2NmsvBvW1Gou3t4Q44i5JcO.tXvBssJz7b.9t63c78C', true, 0, null);
+-- Password is 'password123'
